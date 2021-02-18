bdc4fd592fc75ec42162cffd1dd173d674b58c66
Jefferson Brito <jeffersonrnb@gmail.com>
2021-02-18 01:13:06 -0300

    Adds some rescues

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -4,15 +4,27 @@ module GithubHelper
   @server = Rails.configuration.application[:GITHUB_HOST]
 
   def self.all(since = nil)
-    return RestClient.get("#{@server}/users") if since.nil?
-    RestClient.get("#{@server}/users", params: { since: since })
+    begin
+      return RestClient.get("#{@server}/users") if since.nil?
+      RestClient.get("#{@server}/users", params: { since: since })
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
   end
 
   def self.details(username)
-    RestClient.get("#{@server}/users/#{username}")
+    begin
+      RestClient.get("#{@server}/users/#{username}")
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
   end
 
   def self.repos(username)
-    RestClient.get("#{@server}/users/#{username}/repos")
+    begin
+      RestClient.get("#{@server}/users/#{username}/repos")
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
   end
 end
